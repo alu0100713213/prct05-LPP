2ab36e91ba9e0a5b0e936752e8b6e23c2b1e57e2
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2015-10-22 17:14:50 +0100

    Creado el fichero Rakefile

---
+++ test/tc_fraccion.rb
@@ -0,0 +1,21 @@
+require 'lib/fraccion'
+#require 'test/unit'
+
+class TestFraccion < Test::Unit::TestCase
+  def setup
+    @first = Fraccion.new(5,10)
+    @second = Fraccion.new(2,6)
+  end
+  def test_simple
+    assert_equal("(5,10)", @first.to_s)    # Test de Identidad
+    #assert_equal("(1,1)", (@origen + @unidad).to_s)    # Test de Suma
+    #assert_equal("(-1,-1)", (-@unidad).to_s)    # Test de Resta
+    assert_equal("(25,50)", (@first*5).to_s)    # Test de Multiplicación
+  end
+  def test_type_check
+    assert_raise(RuntimeError) {Fraccion.new('2','6')}
+  end
+  def test_failure
+    assert_equal("(50,25)", (@origen * 5).to_s, "Producto escalar")
+  end
+end
